8a0ab265eeecbe2ec4a2a961f670dad6b9697660
nandhakumar-kumaran_cdwcorp <nandhakumar.kumaran+cdwcorp@cdw.com>
2026-04-07 11:47:15 +0530

    Updated test classes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders hello world', () => {
+test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/hi/i);
+  const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
